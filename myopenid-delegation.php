9e55ec43695ecc4fe4bb02428c81300e59002006
Ben Atkin <ben@benatkin.com>
2011-09-10 10:04:37 +0000

    initial implementation

---
+++ myopenid-delegation.php
@@ -0,0 +1,64 @@
+<?php
+/*  Copyright 2011  Benjamin Atkin  (email : ben@benatkin.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+Plugin Name: MyOpenID Delegation
+Plugin URI: http://benatkin.com/plugins/myopenid-delegation
+Description: Delegates blog URL to MyOpenID
+Version: 0.0.1
+Author: Ben Atkin
+Author URI: http://benatkin.com/
+License: GPL2
+*/
+
+function myopenid_delegation_settings_api_init() {
+	add_settings_section('myopenid_delegation_account_section',
+		'MyOpenID Delegation',
+		'myopenid_delegation_account_section_callback',
+		'general');
+
+	add_settings_field('myopenid_delegation_account_username',
+		'MyOpenID Username',
+		'myopenid_delegation_account_username_field_callback',
+		'general',
+		'myopenid_delegation_account_section');
+
+	register_setting('general', 'myopenid_delegation_account_username');
+}
+
+add_action('admin_init', 'myopenid_delegation_settings_api_init');
+
+function myopenid_delegation_account_section_callback() {
+	echo '<p>Please enter your MyOpenID username so this plugin can add the correct meta tags to the root of your site.</p>';
+}
+
+function myopenid_delegation_account_username_field_callback() {
+	echo '<input name="myopenid_delegation_account_username" id="myopenid_delegation_account_username" type="text" value="' . esc_attr(get_option('myopenid_delegation_account_username')) . '" size=20 maxlength=40 > .myopenid.com';
+}
+
+function myopenid_delegation_render_meta_tags() {
+	$username = get_option('myopenid_delegation_account_username');
+	echo '<link rel="openid.server" href="http://www.myopenid.com/server" />' . "\n";
+	echo '<link rel="openid.delegate" href="http://' . esc_attr($username) . '.myopenid.com/" />' . "\n";
+	echo '<link rel="openid2.local_id" href="http://' . esc_attr($username) . '.myopenid.com" />' . "\n";
+	echo '<link rel="openid2.provider" href="http://www.myopenid.com/server" />' . "\n";
+	echo '<meta http-equiv="X-XRDS-Location" content="http://www.myopenid.com/xrds?username=' . esc_attr($username) . '.myopenid.com" />' . "\n";
+}
+
+add_action('wp_head', 'myopenid_delegation_render_meta_tags');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== WpPygments ===
+Contributors: benatkin
+Tags: openid, identity, delegate
+Requires at least: 3.2.0
+Tested up to: 3.2
+Stable tag: 0.3.2
+
+This plugin delegates OpenID to MyOpenID.
+
+== Description ==
+
+This plugin adds the required meta tags to delegate OpenID to a http://myopenid.com/ account.
+
+== Installation ==
+
+Use the standard WP installation procedure. After installing, go to the settings to set the
+MyOpenID account.
+
+== Troubleshooting ==
+
+If rewrite rules from a caching plugin, or custom rewrite rules, cause the page to do a 301
+redirect, login will fail, at least for some sites. Remove these rewrites to fix it.
